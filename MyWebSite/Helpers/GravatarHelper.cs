8da625ac6a63b6dce280b11d083f171b71cad060
Mark Pollard <mark.pollard@trafigura.com>
2014-08-17 00:26:20 +0100

    First Commit

---
+++ MyWebSite/Helpers/GravatarHelper.cs
@@ -0,0 +1,43 @@
+﻿namespace MyWebSite.Helpers
+{
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public static class GravatarHelper
+    {
+        public static HtmlString GravatarImage(this HtmlHelper htmlHelper, string emailAddress, GravatarOptions options = null)
+        {
+            if (options == null)
+                options = GravatarOptions.GetDefaults();
+
+            var imgTag = new TagBuilder("img");
+
+            emailAddress = string.IsNullOrEmpty(emailAddress) ? string.Empty : emailAddress.Trim().ToLower();
+
+            imgTag.Attributes.Add(
+                "src",
+                string.Format(
+                    "http://www.gravatar.com/avatar/{0}?s={1}{2}{3}",
+                    GetMd5Hash(emailAddress),
+                    options.Size,
+                    "&d=" + options.DefaultImageType,
+                    "&r=" + options.RatingLevel));
+
+            return new HtmlString(imgTag.ToString(TagRenderMode.SelfClosing));
+        }
+
+        // Source: http://msdn.microsoft.com/en-us/library/system.security.cryptography.md5.aspx
+        private static string GetMd5Hash(string input)
+        {
+            byte[] data = MD5.Create().ComputeHash(Encoding.UTF8.GetBytes(input));
+            var builder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                builder.Append(data[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+}
---
+++ MyWebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyWebSite.Startup))]
+namespace MyWebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
