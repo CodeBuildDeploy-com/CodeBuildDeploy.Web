312e5f9903e75c2702ceb367288c880faf3e78cf
Mark Pollard <mark.pollard@trafigura.com>
2015-03-19 16:51:47 +0000

    Add Controller

---
+++ MyWebSite/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MyWebSite.Controllers
+{
+    public class BlogController : Controller
+    {
+        public ActionResult PowerShellRemoting()
+        {
+            return View();
+        }
+    }
+}
