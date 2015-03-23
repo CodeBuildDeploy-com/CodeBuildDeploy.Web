4ee1ead35bb0d0d1273f1d6e86f21b9445249b0c
Mark Pollard <mark.pollard@bjss.co.uk>
2015-03-23 22:26:55 +0000

    Blog Data Access

---
+++ MyWebSite.DataAccess/DAContext.cs
@@ -0,0 +1,23 @@
+﻿namespace MyWebSite.DataAccess
+{
+    using System.Data.Entity;
+    using System.Data.Entity.ModelConfiguration.Conventions;
+
+    public class DAContext : DbContext
+    {
+        public DAContext()
+            : base("name=DefaultConnection")
+        {
+            Database.SetInitializer<DbContext>(null);
+        }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Database does not pluralize table names
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ MyWebSite.DataAccess/Post.cs
@@ -0,0 +1,34 @@
+﻿namespace MyWebSite.DataAccess
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Post
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public virtual int Id { get; set; }
+
+        [StringLength(50)]
+        public virtual string Title { get; set; }
+
+        [StringLength(50)]
+        public virtual string ShortDescription { get; set; }
+
+        [StringLength(150)]
+        public virtual string Description { get; set; }
+
+        public virtual string Content { get; set; }
+
+        public virtual bool Published { get; set; }
+
+        public virtual DateTime PostedOn { get; set; }
+
+        public virtual DateTime? Modified { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        public virtual IList<Tag> Tags { get; set; }
+    }
+}
