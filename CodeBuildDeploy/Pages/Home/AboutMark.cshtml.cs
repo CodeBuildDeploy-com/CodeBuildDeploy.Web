ce4931f429fb105be06ecbe6b416ae4f1c497bbc
Mark Pollard <mark.pollard@bjss.co.uk>
2023-11-10 10:53:34 +0000

    Move to be more CBD centric rather than my blog

---
+++ CodeBuildDeploy/Pages/Home/AboutMark.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace CodeBuildDeploy.Pages.Home;
+
+public class AboutMarkModel : PageModel
+{
+    private readonly ILogger<AboutMarkModel> _logger;
+
+    [ViewData]
+    public string Title { get; } = "About Mark Pollard";
+
+    [ViewData]
+    public string Message { get; } = "The founder of Code Build Deploy..";
+
+    public AboutMarkModel(ILogger<AboutMarkModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
