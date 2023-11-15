8735a87eecd17cfb6baf6d2440ace6c331033ae7
Mark Pollard <mark.pollard@bjss.co.uk>
2023-11-15 17:05:13 +0000

    Update to new mapped 8080 port

--- run.ps1
+++ run.ps1
@@ -1,1 +1,1 @@
-docker run --rm -it -p 5000:443 --env-file local_variables.env codebuilddeploy.azurecr.io/code-build-deploy:latest
+docker run --rm -it -p 5000:8080 --env-file local_variables.env codebuilddeploy.azurecr.io/code-build-deploy:latest
