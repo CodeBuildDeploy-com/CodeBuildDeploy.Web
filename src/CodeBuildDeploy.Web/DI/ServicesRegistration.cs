238e3fbb430ee0ebf9123b17a55a7135d0619d03
Mark Pollard <mark.pollard@bjss.co.uk>
2024-05-30 19:48:07 +0100

    CBD-14: Split out Identity/Account areas into a dedicated service

---
+++ src/CodeBuildDeploy.Web/DI/ServicesRegistration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CodeBuildDeploy.Web.DI
+{
+    public static class ServicesRegistration
+    {
+        private const string AuthCookieName = ".AspNet.AuthCookie";
+
+        public static IServiceCollection ConfigureAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var dataProtectionBuilder = services.AddDataProtection().SetApplicationName("CodeBuildDeploy");
+            var azureStorageDataProtectionSection = configuration.GetSection("Authentication:DataProtection:AzureStorage");
+            if (azureStorageDataProtectionSection.Exists())
+            {
+                dataProtectionBuilder.PersistKeysToAzureBlobStorage(
+                    azureStorageDataProtectionSection["ConnectionString"],
+                    azureStorageDataProtectionSection["ContainerName"],
+                    azureStorageDataProtectionSection["BlobName"]);
+            }
+
+            services.ConfigureApplicationCookie(options => {
+                options.Cookie.Name = AuthCookieName;
+                options.Cookie.Path = "/";
+            });
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                                      .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return services;
+        }
+    }
+}
